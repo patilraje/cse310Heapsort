e661623e0309a07779459859142f44e8ec883013
patilraje <126375795+patilraje@users.noreply.github.com>
2024-02-16 12:27:56 -0700

    Add files via upload

---
+++ Assignment3.cpp
@@ -0,0 +1,307 @@
+// Assignment: ASU CSE310 HW#3 Spring 2024
+// Name: Raje Patil
+// ASU ID: 1231031453
+// ASU Email Address: rpatil46@asu.edu
+// Description: This program displays a menu of choices to a user
+//              and performs the chosen task. It will keep asking a user to
+//              enter the next choice until the choice of 'Q' (Quit) is entered.
+//             	//---- is where you should add your own code
+//********************************************************************************
+
+#include "MaxHeap.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+void printMenu();
+void heapSort(MaxHeap* oneMaxHeap);
+
+int main()
+{
+	char input1 = 'Z';
+	int vin, newVIN;
+	string model, make;
+    double price;
+	int capacity, index, index2 = -1;   //array capacity and index
+	bool success = false;
+	//int size = 0;
+
+    Car oneCar;
+
+	// instantiate an empty Heap
+	MaxHeap* heap1 = NULL;
+	printMenu();
+
+	do {
+		cout << "\nWhat action would you like to perform?" << endl;
+		cin.get(input1);
+		input1 = toupper(input1);
+		cin.ignore(20, '\n');	//flush the buffer
+
+		// matches one of the cases
+		switch (input1)
+		{
+			case 'C':	//create empty Heap with the relevant capacity
+				cout << "\nPlease enter the heap capacity: ";
+				cin >> capacity;
+				cin.ignore(20, '\n');	//flush the buffer
+				heap1 = new MaxHeap(capacity);  // Dynamically allocate memory for a new MaxHeap object
+				break;
+
+
+			case 'D':	//delete the heap, call the destructor explicitly
+				cout << "\nDelete the heap" << endl;
+				//heap1->~MaxHeap();
+				delete heap1;  // Use delete to free the memory allocated for the heap
+				heap1 = NULL;  // Set the pointer to NULL after deleting
+
+				//re-initialize it with capacity 5
+				heap1 = new MaxHeap(5);
+				break;
+
+
+
+			case 'E':	//Extract the maximum node
+                if (heap1 == NULL)
+                    cout << "\nEmpty heap, can NOT extract max" << endl;
+                else
+                {
+                    cout << "\nBefore extract heap max operation:" << endl;
+					heap1->printHeap();
+					heap1->extractHeapMax();
+					cout << "\nAfter extract heap max operation:" << endl;
+					heap1->printHeap();
+                }
+				break;
+
+
+			case 'F':	//Find a Car
+				cout << "\nEnter the car VIN you want to search: ";
+				cin >> vin;
+				cin.ignore(20, '\n');	//flush the buffer
+				index = heap1->isFound(vin);
+
+				if(index != -1)
+				{
+					cout << "\nCar with VIN: " << vin << " is found" << endl;
+					break;
+				}
+
+				else
+				{
+					cout << "\nCar with VIN: " << vin << " is NOT found" << endl;
+					break;
+				}
+				
+
+
+			case 'I':	//Insert a Car
+				cout << "\nEnter the car model: ";
+				cin >> model;
+
+				cout << "\nEnter the car make: ";
+				cin >> make;
+
+				cout << "\nEnter the car VIN: ";
+				cin >> vin;
+				cout << "\nEnter the car price: ";
+				cin >> price;
+				cin.ignore(20, '\n');	//flush the buffer
+
+				//checking if a car with the new vin already exists in the heap
+				index = heap1 -> isFound(vin);
+				if(index != -1)  //if it exists it will return a value > 0 not -1
+				{
+					cout << "\nDuplicated Car. Not added" << endl;
+					cout << "\nCar \"" << model << " " << make << "\" is NOT added" << endl;
+					break;
+				}
+
+				success = heap1 -> heapInsert(vin, model, make, price);
+
+				if(success == true)
+				{
+					cout << "\nCar \"" << model << " " << make << "\" is added" << endl;
+				}
+
+				else
+				{
+					cout << "\nCar \"" << model << " " << make << "\" is NOT added" << endl;
+				}
+
+				break;
+
+
+			//INCOMPLETE
+			case 'K':	//increase the vin
+				cout << "\nEnter the old car VIN you want to increase: ";
+				cin >> vin;
+				cout << "\nEnter the new car VIN: ";
+				cin >> newVIN;
+				cin.ignore(20, '\n');	//flush the buffer
+
+				if(newVIN < vin)
+				{
+					cout << "\nIncrease VIN error: new vin is smaller than current vin" << endl;
+					break;
+				}
+
+
+				index = heap1 -> isFound(vin);
+				if(index == -1)
+				{
+					cout << "\nThe old VIN you try to increase does not exist" << endl;
+					break;
+				}
+
+				//checking if a car with the new vin already exists in the heap
+				index2 = heap1 -> isFound(newVIN);
+				if(index2 != -1)  //if it exists it will return a value > 0 not -1
+				{
+					cout << "\nThe new VIN you entered already exist, increase VIN operation failed" << endl;
+					break;
+				}
+
+				cout << "\nBefore increase VIN operation:" << endl;
+			    heap1 -> printHeap();
+
+				// Create a new car with the updated VIN
+				oneCar = heap1->getCarArr()[index];
+				oneCar.vin = newVIN;
+
+				success = heap1 -> heapIncreaseVIN(index, oneCar);
+				if(success == true)
+				{
+					cout << "\nCar with old VIN: " << vin << " is increased to new VIN: " << newVIN << endl;
+					cout << "\nAfter increase VIN operation: " << endl;
+					heap1 -> printHeap();
+				}
+
+				break;
+
+
+			case 'M':	//get maximum node
+				if(heap1 == NULL)
+				{
+					cout << "\nEmpty heap, can NOT get max node" << endl;
+					break;
+				}
+
+				cout << "\nThe maximum heap node is:" << endl;
+				oneCar = heap1 -> getHeapMax();
+				cout << left;
+				cout << setw(8) << oneCar.vin
+					<< setw(12) << oneCar.model
+					<< setw(12) << oneCar.make
+					<< setw(8) << fixed << setprecision(2) << oneCar.price << endl;
+
+				break;
+
+
+			case 'P':	//Print heap contents
+				if(heap1 == NULL)
+				{
+					cout << "\nEmpty heap, no elements" << endl;
+					break;
+				}
+
+				//heapSort(heap1);
+				heap1 ->printHeap();
+				break;
+
+
+			case 'S':	//HeapSort
+				cout << "\nHeap sort. Cars will be sorted in increasing order of their VINs" << endl;
+				heapSort(heap1);
+				break;
+
+
+			case 'Q':	//Quit
+				//size=0;
+				delete heap1;
+				break;
+
+
+			case '?':	//Display Menu
+				printMenu();
+				break;
+
+
+			default:
+				cout << "Unknown action\n";
+				break;
+		}
+	} while (input1 != 'Q');
+	return 0;
+}
+
+//*****************************************************
+//Given a max heap, we want to sort it in increasing order according to Car VIN, put the
+//sorting result inside another array and print it out
+// Swap two Car objects
+void swapCars(Car& car1, Car& car2) 
+{
+    Car temp = car1;
+    car1 = car2;
+    car2 = temp;
+}
+
+// Given a max heap, we want to sort it in increasing order according to Car VIN
+void heapSort(MaxHeap* oneMaxHeap) 
+{
+    int n = oneMaxHeap->getSize();
+    Car* arr = oneMaxHeap->getCarArr();
+    int originalsize = n;   //to preserve the original size
+
+    // Build a max heap from the input data
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        oneMaxHeap->heapify(i);
+    }
+
+    // One by one extract an element from the heap
+    for (int i = n - 1; i > 0; i--)
+    {
+        // Move the current root to the end
+        swapCars(arr[0], arr[i]);
+        --n;
+        oneMaxHeap -> setSize(n);
+        // Call heapify on the reduced heap
+        oneMaxHeap->heapify(0);
+
+    }
+
+    // Print the sorted array
+    for (int i = 0; i < originalsize; i++)
+    {
+        // Printing car details
+    	cout << left;
+    	cout << setw(8) << arr[i].vin
+    	     << setw(12) << arr[i].model
+    	     << setw(12) << arr[i].make
+    	     << setw(8) << fixed << setprecision(2) << arr[i].price << endl;
+
+    }
+}
+
+
+
+//**The function displays the menu to a user**
+void printMenu()
+{
+	cout << "Choice\t\tAction\n";
+	cout << "------\t\t------\n";
+	cout << "C\t\tCreate a heap\n";
+	cout << "D\t\tDelete the heap\n";
+	cout << "E\t\tExtract max node\n";
+	cout << "F\t\tFind a Car by VIN\n";
+	cout << "I\t\tInsert a Car\n";
+	cout << "K\t\tIncrease the VIN\n";
+	cout << "M\t\tGet the max node\n";
+	cout << "P\t\tPrint the heap\n";
+	cout << "S\t\tHeap Sort\n";
+	cout << "Q\t\tQuit\n";
+	cout << "?\t\tDisplay Help\n\n";
+}
---
+++ MaxHeap.h
@@ -0,0 +1,287 @@
+// Assignment: ASU CSE310 HW#3 Spring 2024
+// Your Name: Raje Patil
+// ASU ID: 1231031453
+// ASU Email address: rpatil46@asu.edu
+// Description: Header file for max heap that has all the functions for heapsort and priority queue functions.
+//				a newly added function apart from the structure was setSize that alters the size of the array while
+//				heapifying to implement a heapsort.
+//********************************************************
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+//Each Car will have a unique Vin
+struct Car
+{
+	string model, make;
+	int vin;        //Vehicle Identification Number
+	double price;
+};
+
+//class MaxHeap represents a max heap that contains Car objects. The underline data structure
+//is a one dimensional array of Cars.
+class MaxHeap
+{
+private:
+	struct Car* carArr;	//an array of Cars
+	int capacity, size;
+
+public:
+	MaxHeap(int capacity);
+	~MaxHeap();
+
+	Car* getCarArr();
+	int getSize();
+	void setSize(int newSize);
+	int getCapacity();
+	int isFound(int aVin);
+	bool heapIncreaseVIN(int index, Car oneCarWithNewVIN);
+	bool heapInsert(int vin, string model, string make, double price);
+	void heapify(int index);
+	Car getHeapMax();
+	void extractHeapMax();
+	int leftChild(int parentIndex);
+	int rightChild(int parentIndex);
+	int parent(int childIndex);
+	void printHeap();
+};
+
+//Constructor to dynamically allocate memory for a heap with the specified capacity
+MaxHeap::MaxHeap(int capacity) 
+{
+	// Initialize the class member variable 'capacity'
+    this->capacity = capacity; 
+    // Initialize the class member variable 'size' to 0
+    this->size = 0;        
+
+    // Dynamically allocate memory for the heap array
+    carArr = new Car[this->capacity];  // Allocate memory for an array of 'Car' objects
+}
+
+
+//Destructor
+//Before termination, the destructor is called to free the associated memory occupied by the heap.
+//and prints the number of nodes deleted by it.
+MaxHeap::~MaxHeap()
+{
+	delete[] carArr;
+	cout << "\nThe number of deleted cars is: " << size << endl;
+}
+
+
+//This is the accessor of the instance variable carArr
+Car* MaxHeap::getCarArr()
+{
+	return carArr;
+}
+
+
+
+int MaxHeap::getSize()
+{
+	return size;
+}
+
+
+void MaxHeap::setSize(int newSize)
+{
+	this -> size = newSize;
+}
+
+
+int MaxHeap::getCapacity()
+{
+	return capacity;
+}
+
+
+int MaxHeap::isFound(int aVin)
+{
+	for(int i=0; i<size; i++)
+	{
+		if(carArr[i].vin == aVin)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+//increase the Car's VIN located at the specific index
+bool MaxHeap::heapIncreaseVIN(int index, Car oneCarWithNewVIN)
+{
+    if (index >= size)
+    {
+        cout << "\nIncrease VIN error: index out of range" << endl;
+        return false;
+    }
+
+    // Add new car at index
+    carArr[index] = oneCarWithNewVIN;
+
+    // Check if the parent is less than the child (and ensure index > 0)
+    while (index > 0 && carArr[parent(index)].vin < carArr[index].vin)
+    {
+        //buffer holds the parent while exchanging these two nodes (parent is smaller, child is larger)
+        Car buffer = carArr[parent(index)];
+        carArr[parent(index)] = carArr[index];
+        carArr[index] = buffer;
+        index = parent(index);  // Move up to the parent
+    }
+
+    return true;
+}
+
+
+
+bool MaxHeap::heapInsert(int vin, string model, string make, double price)
+{
+	//check if the heap is at capacity & double it if required
+	if (size == capacity)
+	{
+		//double the capacity and dynamically reallocate memory
+		MaxHeap::capacity *= 2;
+		Car* newCarArr = new Car[capacity];
+
+		//copy existing elements to the new array
+		for (int i = 0; i < size; ++i)
+		{
+			newCarArr[i] = carArr[i];
+		}
+
+		//delete the old array and update the pointer
+		delete[] carArr;
+		carArr = newCarArr;
+
+		// Display a message indicating the new capacity
+		cout << "\nReach the capacity limit, double the capacity now." << endl;
+		cout << "\nThe new capacity now is " << capacity << endl;
+	}
+
+	//create a dummy car to add to the heap
+	Car dummyCar;
+	dummyCar.vin = -10000;
+	dummyCar.model = "";
+	dummyCar.make = "";
+	dummyCar.price = 0.0;
+
+	// Create a new Car object with the given information
+	Car newCar;
+	newCar.vin = vin;
+	newCar.model = model;
+	newCar.make = make;
+	newCar.price = price;
+
+	// Update the size
+	++size;
+
+	// Insert the new Car object at the rightmost position in the last level
+	carArr[size-1] = dummyCar;
+
+	// "Float" the new Car object up to maintain the max-heap property
+	bool flag = heapIncreaseVIN(size-1, newCar);
+	//when the new car is inserted, delete the dummy car
+
+	return flag;
+}
+
+
+void MaxHeap::heapify(int index)
+{
+	int leftChildIndex = leftChild(index);
+	int rightChildIndex = rightChild(index);
+	int largest = index;
+
+	// Compare with left child based on VIN
+	if (leftChildIndex < size && carArr[leftChildIndex].vin > carArr[largest].vin)
+	{
+		largest = leftChildIndex;
+	}
+
+	// Compare with right child based on VIN
+	if (rightChildIndex < size && carArr[rightChildIndex].vin > carArr[largest].vin) {
+		largest = rightChildIndex;
+	}
+
+	// If the largest is not the current index, swap and recursively heapify the affected subtree
+	if (largest != index) {
+		std::swap(carArr[index], carArr[largest]);
+		heapify(largest);  // Recursively heapify the affected subtree
+	}
+}
+
+
+Car MaxHeap::getHeapMax()
+{
+	return carArr[0];
+}
+
+
+
+void MaxHeap::extractHeapMax()
+{
+    if (size < 1)
+    {
+        cout << "Heap underflow";
+        return;
+    }
+
+    Car max = carArr[0];
+    carArr[0] = carArr[size - 1];
+    carArr[size - 1] = max;  //swap root with the leaf node
+    --size;
+    //delete max;
+    heapify(0);  //heapify the leaf node
+    //return max;
+}
+
+
+
+//returns left child of the parent
+int MaxHeap::leftChild(int parentIndex)
+{
+	return (2 * parentIndex) + 1;
+}
+
+//returns right side of the parent
+int MaxHeap::rightChild(int parentIndex)
+{
+	return (2 * parentIndex) + 2;
+}
+
+
+// Returns parent of a child node
+int MaxHeap::parent(int childIndex)
+{
+    return (childIndex-1) / 2;
+}
+
+
+//prints the heap in order
+void MaxHeap::printHeap()
+{
+	if(MaxHeap::size==0)
+	{
+		cout << "Empty heap, no elements";
+	}
+
+	else
+	{
+		cout << "\nHeap capacity = " << capacity << endl;
+		cout << "\nHeap size = " << size << endl;
+		cout << endl;  // Add an empty line
+
+		for(int i=0; i<=MaxHeap::size-1; i++)
+		{
+			cout << left;
+			cout << setw(8) <<  carArr[i].vin
+					<< setw(12) << carArr[i].model
+					<< setw(12) << carArr[i].make
+					<< setw(8) << fixed << setprecision(2) << carArr[i].price << endl;
+		}
+	}
+
+}
